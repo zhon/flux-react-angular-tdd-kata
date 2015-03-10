01b4bb6d1154867ae50d1ab2531a6af93e54112f
zhon <zhon@xputah.org>
2015-03-10 10:46:06 -0600

    Using example react-news

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,131 @@
+'use strict';
+
 var gulp = require('gulp')
 , browserify = require('browserify')
 , concat = require('gulp-concat')
 , reactify = require('reactify')
 , source = require("vinyl-source-stream")
- 
-gulp.task('browserify', function(){
-  var b = browserify();
-  b.transform(reactify); // use the reactify transform
-  b.add('./src/js/app.js');
-  return b.bundle()
-    .pipe(source('app.js'))
-    .pipe(gulp.dest('./dist/js'));
+, watchify   = require('watchify')
+, $          = require('gulp-load-plugins')()
+;
+
+console.log($);
+
+var prod = $.util.env.prod;
+
+// gulp-plumber for error handling
+function onError() {
+    /* jshint ignore:start */
+    var args = Array.prototype.slice.call(arguments);
+    $.util.beep();
+    $.notify.onError({
+        title: "Compile Error",
+        message: "<%= error.message %>"
+    }).apply(this, args);
+    this.emit('end'); // Keep gulp from hanging on this task
+    /* jshint ignore:end */
+}
+
+// Styles
+gulp.task('styles', function() {
+    return gulp.src('src/styles/**/*')
+        .pipe($.plumber({
+            errorHandler: onError
+        }))
+        .pipe($.concat('main.scss'))
+        .pipe($.rubySass({
+            style: 'compressed',
+            precision: 10,
+            loadPath: ['src/bower_components']
+        }))
+        .pipe($.autoprefixer('last 3 versions'))
+        .pipe(gulp.dest('dist/styles'))
+        .pipe($.size());
 });
- 
-gulp.task('copy', function() {
-	gulp.src('src/index.html')
-		.pipe(gulp.dest('dist'))
-})
- 
-gulp.task('default', ['browserify','copy'])
- 
-gulp.task('watch', function() {
-	gulp.watch('src/**/*.*', ['default'])
-})
+
+// Scripts
+gulp.task('scripts', function() {
+    var bundler;
+    bundler = browserify({
+        basedir: __dirname,
+        noparse: ['react/addons', 'reflux', 'fastclick', 'react-router'],
+        entries: ['./src/scripts/app.jsx'],
+        transform: [reactify],
+        extensions: ['.jsx'],
+        debug: true,
+        cache: {},
+        packageCache: {},
+        fullPaths: true
+    });
+
+    bundler = watchify(bundler);
+
+    function rebundle() {
+        console.log('Bundling Scripts...');
+        var start = Date.now();
+        return bundler.bundle()
+            .on('error', onError)
+            .pipe(source('app.js'))
+            .pipe(prod ? $.streamify($.uglify()) : $.util.noop())
+            .pipe(gulp.dest('dist/scripts'))
+            .pipe($.notify(function() {
+                console.log('Bundling Complete - ' + (Date.now() - start) + 'ms');
+            }));
+    }
+
+    bundler.on('update', rebundle);
+
+    return rebundle();
+});
+
+// HTML
+gulp.task('html', function() {
+    return gulp.src('src/*.html')
+        .pipe($.useref())
+        .pipe(gulp.dest('dist'))
+        .pipe($.size());
+});
+
+
+// Images
+gulp.task('images', function() {
+    return gulp.src('src/images/**/*')
+        .pipe($.cache($.imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images'))
+        .pipe($.size());
+});
+
+
+// Webserver
+gulp.task('serve', function() {
+    gulp.src('dist')
+        .pipe($.webserver({
+            livereload: true,
+            port: 9000,
+            fallback: 'index.html'
+        }));
+});
+
+
+// Clean
+gulp.task('clean', function () {
+  return gulp.src('dist', {read: false})
+    .pipe($.clean());
+});
+
+
+// Default task
+gulp.task('default', ['clean', 'html', 'styles', 'scripts']);
+
+
+// Watch
+gulp.task('watch', ['html', 'styles', 'scripts', 'serve'], function() {
+    gulp.watch('src/*.html', ['html']);
+    gulp.watch('src/styles/**/*.scss', ['styles']);
+    gulp.watch('src/images/**/*', ['images']);
+});
+
